c86f1c248bc67e003152aee93394ae3a89e7584b
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-02-28 21:31:29 -0500

    Add clock script; update todo; HANDS NEED WORK

--- Assets/Peter/GameManager/Clock/Clock.cs
+++ Assets/Peter/GameManager/Clock/Clock.cs
@@ -2,17 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Used to show remaining Round Time
+/// An analog clock, with hands
+/// NOTICE ONLY USING MINUTE HAND, HOUR HAND NOT IMPLEMENTED
+/// </summary>
 public class Clock : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // We rotate this about the face
+    public GameObject minuteHand;
+
+    // We also rotate this about the face, albeit more slowly
+    // TODO FUTURE
+    // public GameObject hourHand;
+
+    // Who we notify on round over
+    public GameManager gameManager;
+
+    // What time remains right now
+    public float currentTime;
+
+    // What time we start with
+    public float startTime;
+
+    // When we are not runnin
+    private bool stopped;
+
     void Start()
     {
-        
+        // Reset the time
+        currentTime = startTime;
+
+        // Set the alarm
+        stopped = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // If we should be running
+        if (currentTime > 0.0f)
+        {
+            // Remove time while time remains
+            currentTime -= Time.deltaTime;
+
+            // Update the Clock Hands
+            // TODO
+        }
+
+        // Do something only once, when time stops
+        if (!stopped)
+        {
+            TimeOut();
+        }
     }
+
+    /// <summary>
+    /// Trigger effects, etc to signify time is up
+    /// </summary>
+    private void TimeOut()
+    {
+        // Alert the game manager the time is up
+        gameManager.RoundOver();
+    }
+
 }
