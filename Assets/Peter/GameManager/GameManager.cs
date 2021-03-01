7c3c118c0f387412a9408c4a3610280951ad02f5
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-02-28 21:08:44 -0500

    Add Game Manager Functions and Variables and Documentation

--- Assets/Peter/GameManager/GameManager.cs
+++ Assets/Peter/GameManager/GameManager.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Image/Text/Audio activated on Game Over
+    public GameObject gameOver;
+
+    // Image/Text/Audio activated on Round Over
+    public GameObject roundOver;
+
+    /// <summary>
+    /// Player has started the game
+    /// </summary>
+    public void Start()
     {
-        
+        // Ensure the GameOver action is hidden
+        gameOver.SetActive(false);
+
+        // Ensure the RoundOver action is hidden
+        roundOver.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Player has failed the game
+    /// </summary>
+    public void OnGameOver()
     {
-        
+        // Show the GameOver action
+        gameOver.SetActive(true);
+    }
+
+    /// <summary>
+    /// Player has successfully survived a round
+    /// </summary>
+    public void RoundOver()
+    {
+        // Show the RoundOver action
+        roundOver.SetActive(true);
+    }
+
+    /// <summary>
+    /// Player has started a new round
+    /// </summary>
+    public void RoundStart()
+    {
+        roundOver.SetActive(false);
     }
 }
