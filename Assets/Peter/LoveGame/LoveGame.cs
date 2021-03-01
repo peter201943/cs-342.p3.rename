18932be8f1f6739b1e572f7767d683b9c3ec4198
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-02-28 21:58:16 -0500

    Finished adding variables to love game; now needs logic

--- Assets/Peter/LoveGame/LoveGame.cs
+++ Assets/Peter/LoveGame/LoveGame.cs
@@ -11,20 +11,23 @@ using UnityEngine;
 /// 
 /// TODO FUTURE
 /// * Add a timer/slider at bottom of screen showing how much time to answer a question remains
+/// * Adjust the max love points to be smaller and smaller over time - also add effects to indicate such!
 /// </summary>
 public class LoveGame : MonoBehaviour
 {
-    [Header("Misc Settings")]
-    // The current order of sessions
-    public List<GameObject> sessionQueue;
-    // All possible sessions
-    public List<GameObject> sessions;
-    // The active dialog session
-    public GameObject currentSession;
+    [Header("The points a player has")]
     // What the player acrrues playing the game
-    public int score;
+    public int lovePointsCurrent;
+    // What value the player starts the game with
+    public int lovePointsStart;
+    // The most points a player can get (part of difficulty?)
+    public int lovePointsMax;
+
+    [Header("External References")]
     // Who we update with our score
     public Bar loveBar;
+    // Who we notify of GameOver, etc
+    public GameManager gameManager;
 
     [Header("Question Spawning Difficulty")]
     // The max time a question can take to spawn
@@ -42,6 +45,14 @@ public class LoveGame : MonoBehaviour
     // How quickly questions take less time to be shown
     public float answerDelayDecrementRate;
 
+    [Header("Selection Choosing")]
+    // The current order of sessions
+    public List<GameObject> sessionQueue;
+    // All possible sessions
+    public List<GameObject> sessions;
+    // The active dialog session
+    public GameObject currentSession;
+
     /// <summary>
     /// Setup the Love Minigame
     /// </summary>
@@ -55,8 +66,26 @@ public class LoveGame : MonoBehaviour
 
         // Pick our first Session
         // TODO
+
+        // Reset the Score
+        lovePointsCurrent = lovePointsStart;
     }
 
+    /// <summary>
+    /// Player answers a question correctly
+    /// </summary>
+    public void Correct()
+    {
+        // TODO
+    }
+
+    /// <summary>
+    /// Player answers a question incorrectly
+    /// </summary>
+    public void InCorrect()
+    {
+        // TODO
+    }
 
 
 }
