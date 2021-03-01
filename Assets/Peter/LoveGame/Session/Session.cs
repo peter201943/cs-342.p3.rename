75aba6dd019c3352d2e20ae17e67d9f0c3f7f1a2
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-02-28 23:25:16 -0500

    Add session script

--- Assets/Peter/LoveGame/Session/Session.cs
+++ Assets/Peter/LoveGame/Session/Session.cs
@@ -10,5 +10,72 @@ using UnityEngine;
 /// </summary>
 public class Session : MonoBehaviour
 {
+    // Who we notify
+    public LoveGame loveGame;
+    // all of the answers
+    // Which answers are ok
+    public List<Answer> goodAnswers;
+    // Time remaining to answer the question
+    public float currentTime;
+    // Are we done?
+    public bool done;
 
+    /// <summary>
+    /// Setup fo simple state management
+    /// </summary>
+    private void Start()
+    {
+        // Reset state
+        done = False;
+    }
+
+    /// <summary>
+    /// When player fails to answer on time
+    /// </summary>
+    private void TimeOut()
+    {
+        loveGame.InCorrect();
+        // TODO FUTURE
+        // Any side effects for being slow
+        return;
+    }
+
+    /// <summary>
+    /// Update the Timer
+    /// </summary>
+    private void Update()
+    {
+        if (currentTime > 0.0f)
+        {
+            currentTime -= Time.deltaTime;
+        }
+
+        if (currentTime <= 0.0f)
+        {
+            TimeOut();
+        }
+    }
+
+    /// <summary>
+    /// When one of the answers reports being clicked
+    /// </summary>
+    /// <param name="sender"></param>
+    public void Answer(GameObject sender)
+    {
+        // Check if it is a correct answer
+        foreach (Answer goodAnswer in goodAnswers)
+        {
+            if (sender == goodAnswer)
+            {
+                loveGame.Correct();
+                // TODO FUTURE
+                // Any Side Effects for being correct
+                return;
+            }
+        }
+        loveGame.InCorrect();
+        // TODO FUTURE
+        // Any side effects for being wrong
+        return;
+    }
 }
