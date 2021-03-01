da8b9f09fe0d88f22e9e842811e5358f76655e30
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-02-28 21:42:06 -0500

    Add first lines to lovegame; add details to todo

--- Assets/Peter/LoveGame/LoveGame.cs
+++ Assets/Peter/LoveGame/LoveGame.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This is 1 of 2 minigames
+/// It involves mouse interaction/clicking on dialog bubbles
+/// It draws from a set of prewritten questions
+/// And displays them based off of timers
+/// </summary>
 public class LoveGame : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    // The current order of sessions
+    public List<GameObject> sessionQueue;
+
+    // All possible sessions
+    public List<GameObject> sessions;
+
+    // The active dialog session
+    public GameObject currentSession;
+
+
     void Start()
     {
-        
+        // Gather any child Sessions and list them
+
+        // Setup the Sessions Queue
+
+        // Pick our first Session
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
