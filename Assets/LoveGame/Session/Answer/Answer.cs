1263dd873e65a771886700e24926031415d82c11
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-03-07 17:19:55 -0500

    Change answer type from click to collide; update peter scene with new date and pull from austin

--- Assets/LoveGame/Session/Answer/Answer.cs
+++ Assets/LoveGame/Session/Answer/Answer.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 /// <summary>
 /// Should have a textbox
 /// And be configured in Session to be designated right or wrong answer
+/// 
+/// NOTICE 2021-03-07
+/// Major change to how this is handled
+/// Answers will now fall down and need to be navigated to
+/// They are triggered by collisions
+/// <see href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnCollisionEnter2D.html">MonoBehaviour.OnCollisionEnter2D(Collision2D)</see>
 /// </summary>
 public class Answer : MonoBehaviour
 {
@@ -13,15 +19,30 @@ public class Answer : MonoBehaviour
     public Session session;
 
     /// <summary>
-    /// When user clicks on us
+    /// When user hits us (if at all)
     /// </summary>
-    private void OnMouseDown()
+    void OnCollisionEnter2D(Collision2D col)
     {
         // TODO FUTURE
         // Play any effects
 
+        // TEMP DEBUG
+        Debug.Log("Was Collided");
+
         // Notify the session with ourselves
         session.Answer(this.gameObject);
+    }
+
+
+    /// <summary>
+    /// Causes us to fall down
+    /// </summary>
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        gameObject.transform.Translate(moveHorizontal * 0.05f, moveVertical * 0.25f, 0.0f);
     }
 
 }
