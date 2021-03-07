74465cca18d60dafdc146f9656d7a1a96c80b3f1
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-03-07 17:52:50 -0500

    Add out of bounds checks; add variables; add move logic

--- Assets/LoveGame2/Cursor/LoveGameCursor.cs
+++ Assets/LoveGame2/Cursor/LoveGameCursor.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 /// Quick 2D Cursor-Like thing
 /// Uses the A and D or <- and -> keys to move left and right
 /// <see href="https://answers.unity.com/questions/1467659/how-to-detect-a-keyboard-key-held-down.html">How to detect a keyboard key held down?</see>
+/// <see href="https://gamedev.stackexchange.com/questions/152516/what-is-the-script-that-is-needed-to-move-a-2d-spritesquare">what is the script that is needed to move a 2D sprite(square)?</see>
 /// </summary>
 [RequireComponent(typeof(Collider2D))]
 public class LoveGameCursor : MonoBehaviour
@@ -16,19 +17,36 @@ public class LoveGameCursor : MonoBehaviour
     // How far right we can travel
     public float rightMax;
 
+    // How fast we can travel
+    public float moveSpeed;
+
+    // Move Directions
+    private Vector3 leftward  = new Vector3(0, 1, 0);
+    private Vector3 rightward = new Vector3(0, -1, 0);
+
     // Check for Inputs
     private void Update()
     {
         // Move Left
         if ( Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) )
         {
-            // TODO Move Cursor
+            // Do not proceed outside bounds
+            if ( leftMax < transform.position.x )
+            {
+                // Move Cursor Left
+                transform.Translate(leftward * moveSpeed * Time.deltaTime, Space.World);
+            }
         }
 
         // Move Right
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            // TODO Move Cursor
+            // Do not proceed outside bounds
+            if (rightMax < transform.position.x)
+            {
+                // Move Cursor Right
+                transform.Translate(rightward * moveSpeed * Time.deltaTime, Space.World);
+            }
         }
     }
 }
