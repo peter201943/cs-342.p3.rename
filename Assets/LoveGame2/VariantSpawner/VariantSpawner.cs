f419a65f73772d767c3e186b569ff3d73ad38559
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-03-07 18:36:02 -0500

    create stupidly simple variant spawner

---
+++ Assets/LoveGame2/VariantSpawner/VariantSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariantSpawner : MonoBehaviour
+{
+    public List<GameObject> subjects;
+    public float delay = 20f;
+    private float currentDelay = 0.0f;
+    private int next = 0;
+
+    private void Update()
+    {
+        currentDelay -= Time.deltaTime;
+        if ( currentDelay < delay )
+        {
+            currentDelay = delay;
+            next++;
+            if ( next > subjects.Count )
+            {
+                next = 0;
+            }
+            Instantiate( subjects[next], transform.position, Quaternion.identity );
+        }
+    }
+}
