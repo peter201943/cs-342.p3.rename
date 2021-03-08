c370608940f45dc05f6d93206d8f94f19f9fd609
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2021-03-08 00:05:46 -0500

    Create loss class for bars

---
+++ Assets/Misc/Loss.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Loss : MonoBehaviour
+{
+    public  Slider  bar;
+    public  float   delay           = 5f;
+    private float   currentDelay    = 5f;
+    public  int     rate            = 2;
+    public  float   delayRate       = 1.5f;
+
+    void Update()
+    {
+        currentDelay -= Time.deltaTime;
+        if ( currentDelay < 0.0f )
+        {
+            currentDelay = delay;
+            delay = delay / delayRate;
+            bar.value = bar.value - rate;
+        }
+    }
+}
